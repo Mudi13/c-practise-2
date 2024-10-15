2e99c931b4be241404576188e10b86914d1cc4f9
MUDIT CHAUHAN <93607579+Mudi13@users.noreply.github.com>
2024-10-15 23:56:47 +0530

    Add files via upload

---
+++ Checking Vowels.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c",&ch);
+    
+    if(ch=='A'|| ch=='a'|| ch=='E'|| ch=='e'|| ch=='I'|| ch=='i'|| ch=='O'|| ch=='o'|| ch=='U'|| ch=='u')
+    {
+        printf("%c is a vowel",ch);
+    }
+    else
+    {
+        printf("Not a vowel");
+    }
+}
